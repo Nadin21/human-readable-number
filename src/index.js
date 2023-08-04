42dc42a456cd5f75aed8a3a51c3c7c36e5796d5c
Nadin21 <nadin21dir@gmail.com>
2023-08-04 17:35:27 +0300

    feat: add condition for 0 and hundred

--- src/index.js
+++ src/index.js
@@ -49,7 +49,42 @@ module.exports = function toReadable(number) {
             }
 
         }
-
+        else if (arrayReversNum.length == 1 && arrayReversNum[0] == 0) {
+            result = "zero";
+        }
+        else if (i == 0 || i == 2) {
+            if (i == 2) result = ' hundred ' + result;
+            switch (arrayReversNum[i]) {
+                case '1':
+                    result = "one" + result;
+                    break;
+                case '2':
+                    result = "two" + result;
+                    break;
+                case '3':
+                    result = "three" + result;
+                    break;
+                case '4':
+                    result = "four" + result;
+                    break;
+                case '5':
+                    result = "five" + result;
+                    break;
+                case '6':
+                    result = "six" + result;
+                    break;
+                case '7':
+                    result = "seven" + result;
+                    break;
+                case '8':
+                    result = "eight" + result;
+                    break;
+                case '9':
+                    result = "nine" + result;
+                    break;
+            }
+            continue;
+        }
        
     }
     return result.replace(/\s$/,'') ;
